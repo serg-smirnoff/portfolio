734306a89b322c88c1ca7117b6a7d2c2ee92bb88
Sergey Smirnov <info@serg-smirnoff.com>
2022-06-28 17:45:24 +0200

    fixed syntaxis mistakes

--- python/price_formated.py
+++ python/price_formated.py
@@ -1,6 +1,6 @@
 def price_formated(p):
     p = '{0:,}'.format(int(p)).replace(',', ' ')
-    return p
+        return p
     mod = round((p % 100),1)
     if mod != 0.0:
         p = '{0:.1f}'.format(p).replace('.', ',')
