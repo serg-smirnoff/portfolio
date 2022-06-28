0ea564fd43dcc62cda91ff350803154632282329
Sergey Smirnov <info@serg-smirnoff.com>
2022-06-28 14:14:30 +0200

    added price_formated.py

---
+++ python/price_formated.py
@@ -0,0 +1,10 @@
+def price_formated(p):
+    p = '{0:,}'.format(int(p)).replace(',', ' ')
+    return p
+    mod = round((p % 100),1)
+    if mod != 0.0:
+        p = '{0:.1f}'.format(p).replace('.', ',')
+        return p
+    else:
+        p = '{0:,}'.format(int(p)).replace(',', ' ')
+        return p
