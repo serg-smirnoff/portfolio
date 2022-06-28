f4a409d86922945a6754267c48055495541e1c95
Sergey Smirnov <info@serg-smirnoff.com>
2022-06-28 17:25:30 +0200

    added fadeIn_effect.css

---
+++ php/drupal/drupal_remove_canonical_from_main.php
@@ -0,0 +1,13 @@
+<?php
+
+Drupal - убрать Canonical на главной
+
+function nazvanietemy_html_head_alter(&$head_elements) {
+  foreach ($head_elements as $key => $element) {
+        if (isset($element['#attributes']['rel']) && $element['#attributes']['rel'] == 'canonical') {
+            unset($head_elements[$key]);
+          }
+     }
+}
+
+?>
