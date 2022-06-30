caca65408ea05d436b4638d12e9115eee9d2e896
Sergey Smirnov <info@serg-smirnoff.com>
2022-06-30 11:00:50 +0200

    added new changes

--- php/scripts for cms/drupal/drupal_remove_canonical_from_main.php
+++ php/scripts for cms/drupal/drupal_remove_canonical_from_main.php
@@ -1,13 +1,12 @@
 <?php
 
-Drupal - убрать Canonical на главной
+// Drupal - убрать Canonical на главной
 
 function nazvanietemy_html_head_alter(&$head_elements) {
-  foreach ($head_elements as $key => $element) {
-        if (isset($element['#attributes']['rel']) && $element['#attributes']['rel'] == 'canonical') {
-            unset($head_elements[$key]);
-          }
-     }
+    foreach ($head_elements as $key => $element) {
+        if (isset($element['#attributes']['rel']) && $element['#attributes']['rel'] == 'canonical') {
+            unset($head_elements[$key]);
+        }
+    }
 }
-
 ?>
