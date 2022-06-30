bc4a5cb9089ee81f3022b552a4c79738f74bdf1f
Sergey Smirnov <info@serg-smirnoff.com>
2022-06-30 11:27:36 +0200

    added russian_date.php

--- python/filtering_list_without_remove_elements.py
+++ python/filtering_list_without_remove_elements.py
@@ -1,3 +1,3 @@
-// filtering list without remove elements
+# filtering list without remove elements
 
 ret = [ticket for ticket in visible_tickets if ticket.from_date == ticket.to_date or ticket.to_date == None]
