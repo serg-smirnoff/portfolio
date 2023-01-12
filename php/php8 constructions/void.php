bbdb2f2565e4029e2e766a4c714a4a119ab31775
Sergey Smirnov <info@serg-smirnoff.com>
2023-01-12 11:19:00 +0300

    void never return values

---
+++ php/php8 constructions/void.php
@@ -0,0 +1,9 @@
+<?php class voidExampleClass{
+    
+    // void pseudo type = function never return value
+    public function setDiscount(int|float $num) : void {
+        $this->discount = $num;
+    }
+
+}
+?>
