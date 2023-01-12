540caa55da0db729d446e34b920c1d86ef36e48f
Sergey Smirnov <info@serg-smirnoff.com>
2023-01-12 20:12:55 +0300

    added new features

--- php/php zandstra book 8.0/class_example.php
+++ php/php zandstra book 8.0/class_example.php
@@ -1,32 +1,154 @@
 
 <?php 
 
+// public (открытые)
+// можно получать доступ из любого контекста 
+
+// private (закрытые)
+// можно получить доступ только из того класса, в котором они объявлены. 
+// Даже подклассы данного класса не имеют доступа к таким свойствам и методам
+
+// protected (защищенные)
+// можно получить доступ либо из содержащего их класса, либо из его подкласса. 
+// Никакому внешнему коду такой доступ не предоставляется
+
 // class example
 class ShopProduct {    
     
+    // property discount
+    private int|float $discount = 0;
+    
     // constructor
     public function __construct(
-        // properties in constructor (only php 8+)
-        public string $title,
-        public string $producerMainName = "",
-        public string $producerFirstName = "",
-        public int $price = 0){
+                                // properties in constructor (only php 8+)
+                                private string $title,
+                                private string $producerMainName = "",
+                                private string $producerFirstName = "",
+                                protected int|float $price = 0
+                               ){
     }
     
-    // method
-    public function getProducer() {
-        return $this->producerFirstName." ".$this->producerMainName;
+    // method getProducerFirstName()
+    public function getProducerFirstName() : string {
+        return $this->producerFirstName;
+    }
+
+    // method getProducerMainName()
+    public function getProducerMainName(): string {
+        return $this->producerMainName;
+    }
+    
+    // method getDiscount()
+    public function setDiscount(int|float $num): void {
+        $this->discount = $num;
+    }
+    
+    // method setDiscount()
+    public function getDiscount(): int {
+        return $this->discount;
+    }
+    
+    // method getTitle()
+    public function getTitle() : string {
+        return $this->title;
+    }
+
+    // method getPrice()
+    public function getPrice(): int|float {
+        return ($this->price - $this->discount);
+    }
+    
+    // method getProducer()
+    public function getProducer() : string {
+        return $this->producerFirstName ." ". $this->producerMainName;
+    }
+
+    
+    public function getSummaryLine(): string {
+        $base = "{$this->title} ({$this->producerMainName}, {$this->producerFirstName})";
+        return $base;
+    }
+    
+}
+
+// CDProduct extends ShopProduct
+class CDProduct extends ShopProduct{
+    public function __construct(string $title,
+                            string $firstName,
+                            string $mainName,int|float $price,
+                            private int $playLength
+                           ){
+        parent::__construct($title,$firstName,$mainName,$price);
+    }
+    public function getPlayLength () : int{
+        return $this->playLength;
+    }
+    public function getSummaryLine(): string{
+        $base = "{$this->title}({$this->producerMainName}, {$this->producerFirstName})";
+        $base .= "Время звучания - {$this->playLength}";
+        return $base;
     }
 
 }
 
-$product = new ShopProduct(title: "Тридцатая любовь Марины","Владимир","Сорокин",price:750);
-var_dump($product);
+// BookProduct extends ShopProduct
+class BookProduct extends ShopProduct{
+    
+    public function __construct(string $title,
+                                string $firstName,
+                                int|float $price,
+                                string $mainName, 
+                                private int $numPages){
+        parent::__construct ($title,$firstName,$mainName,$price );
+    }
+
+    public function getNumberOfPages() : int{
+        return $this >numPages;
+    }
+    
+    public function getSummaryLine() : string{
+        $base = parent::getSummaryLine();
+        $base .= " : {$this->numPages} стр . ";
+        return $base;
+    }
+    
+    public function getPrice() : int|float{
+        return $this->price;
+    }
+
+}
+
+$product = new ShopProduct("Тридцатая любовь Марины","Владимир","Сорокин",750);
+// var_dump($product);
 
 // var_export($product);
 // print_r($product);
 
-// print "Автор: {$product->producerFirstName} " . " {$product->producerMainName}\n";
-// print $product->getProducer();
+// print "Автор: {$product->getProducerFirstName()} " . " {$product->getProducerMainName}\n";
+print $product->getProducer();
+$product->setDiscount(10);
+print $product->getPrice();
+
+
+// class ShopProductWriter{
+//     private $products = [];
+//     public function addProduct(ShopProduct $shopProduct) : void {
+//         $this->products[] = $shopProduct;
+//     }
+//     public function write () : void {
+//         $str = "";
+//         foreach ($this->products as $shopProduct){
+//             $str .= "{$shopProduct->title}: ";
+//             $str .= $shopProduct->getProducer();
+//             $str .= "({$shopProduct->getPrice()})\n";
+//         }
+//         print $str;
+//     }
+// }
+
+// $sp_writer = new ShopProductWriter();
+
+// $sp_writer->addProduct($product);
+// $sp_writer->write();
 
 ?>
