ded21f8dd1be0a7f7328479714159e533a53b7cd
Sergey Smirnov <info@serg-smirnoff.com>
2023-03-08 15:46:59 +0200

    reformated structure

--- php/algorithms and data structures/конструкции/dinamicVariables.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	$first 	= "second";
-	$second = "third";
-	
-	print $first; // напечатает "second"
-	print $$first; // напечатает "third"
-	
-?>
--- php/php_constructions/reverseCharsInStr.php
+++ php/php_constructions/reverseCharsInStr.php
@@ -1,12 +1,12 @@
-<?
-/*
-	ReverseCharsInStr
-	*/
-	
+<?php
+
+/* ReverseCharsInStr
+*/
+
 $str = "Hello world!";
 
 for ($i = strlen($str); $i >= 0; $i--){
-	$return .= $str[$i];
+    $return .= $str[$i];
 }
 
 echo $return;
