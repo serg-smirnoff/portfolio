260d2af9a8a9148a8ea880d603e3a1a65eee342a
Sergey Smirnov <info@serg-smirnoff.com>
2023-03-08 15:57:38 +0200

    reformated structure

--- facebook.js
+++
@@ -1,8 +0,0 @@
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.async=true;
-  js.src = "//connect.facebook.net/ru_RU/all.js#xfbml=1&amp;appId=173992379348162";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
