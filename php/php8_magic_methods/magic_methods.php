17e6315bd67286f1ab8f4ca2811895fff339cb76
Sergey Smirnov <info@serg-smirnoff.com>
2024-04-13 12:51:34 +0200

    added new magic methods

--- php/php8_magic_methods/magic_methods.php
+++ php/php8_magic_methods/magic_methods.php
@@ -4,6 +4,10 @@ class MagicMethods {
     
     private $data = [];
 
+    public function class_name(): string{
+        return __CLASS__;
+    }
+
     public function __construct() {
         echo "Constructor called!";
     }
@@ -36,67 +40,98 @@ class MagicMethods {
         echo "Arguments: ". implode(', ', $arguments);
     }
 
-    public function __toString() {
-        return "This is an instance of MyClass";
+    public function __toString(){
+        return "This is an instance of " . $this->class_name();
     }
     
-    public function __sleep(){}
+    public function __sleep(){
 
-    public function __wakeup(){}
+    }
 
-    public function __serialize(){}
+    public function __wakeup(){
 
-    public function __unserialize($arg){}
+    }
 
-    public function __invoke(){}
 
-    public static function __set_state($arg){}
+    public function __serialize(){
+
+    }
+
+    public function __unserialize($arg){
+
+    }
+
+    public function __invoke($x){
+        echo $this->class_name() . " class object is called as a function!";
+        echo "<br>";
+        var_dump($x);
+    }
+
+    public static function __set_state($arg){
+
+    }
 
     public function __clone(){}
 
     public function __debugInfo(){}
 
-    public function __destruct() {
+    public function __destruct(){
         echo "Destructor called!";
     }
 
 }
 
 // __construct()
+echo "<strong>__construct()</strong><br>";
 $obj = new MagicMethods();              // Output: Constructor called!
-
-// __set()
-$obj->name = "John";                    // Sets the 'name' property
 
 echo "<br><br>";
 
-// __get()
-echo $obj->name;                        // Output: John    
+// __set()
+echo "<strong>__set()</strong><br>";
+$obj->name = "John";                    // Sets the 'name' property
 
+echo "<br>";
+
+// __get()
+echo "<strong>__get()</strong><br>";
+echo $obj->name;                        // Output: John    
 echo "<br><br>";
 
 // __isset()
-
+echo "<strong>__isset()</strong><br>";
 echo isset($obj->tratata);
 echo "<br><br>";
 
 // __unset()
+echo "<strong>__unset()</strong><br>";
 unset($obj->tratata);
 echo "<br><br>";
 
 // __call()
+echo "<strong>__call()</strong><br>";
 $obj->myMethod("arg1", "arg2");         // Output: Calling method: myMethod Arguments: arg1, arg2
 
 echo "<br><br>";
 
 // __callStatic()
+echo "<strong>__callStatic()</strong><br>";
 MagicMethods::myMethod("arg1", "arg2");// Output: Calling method: myMethod Arguments: arg1, arg2
 
 echo "<br><br>";
 
 // __toString()
+echo "<strong>__toString()</strong><br>";
 echo $obj;                              // Output: This is an instance of MyClass
 
 echo "<br><br>";
 
+// __invoke()
+echo "<strong>__invoke()</strong><br>";
+$obj(5);
+echo "<br>";
+var_dump(is_callable($obj));
+
+echo "<br><br>";
+echo "<strong>__destruct()</strong><br>";
 unset($obj);
